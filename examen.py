d3dfac5bf124862c1b9c84efdad8a702f3ab1a33
tomastorresm <ttorres97294@umanizales.edu.co>
2022-04-29 19:31:58 -0500

    examen

--- examen.py
+++ examen.py
@@ -1,2 +1,5 @@
-print("hola")
-print("examen")
+tamaño=int(input("ingrese el tamaño del array: "))
+for f in range(tamaño):
+  valor=int(input("ingrese el valor de cada uno de los elementos: "))
+  lista=[valor]
+  print(lista)
